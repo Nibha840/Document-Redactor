a26d812276c9732f4986d6d83b81c869aa90e990
Nibha840 <166140139+Nibha840@users.noreply.github.com>
2026-07-17 20:12:37 +0530

    push

--- detector.py
+++ detector.py
@@ -19,6 +19,7 @@ from __future__ import annotations
 
 from typing import List
 
+import config
 from ner_detector import NERDetector
 from regex_detector import RegexDetector
 from presidio_detector import PresidioDetector
